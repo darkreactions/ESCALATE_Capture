e8e060cd6bb27434ec5e333e58d3b6e1cbb2a658
Ian Pendleton <ipendlet@umich.edu>
2018-05-25 13:15:21 -0400

    Create template and building to edit experimental input.

--- Google_IO.py
+++ Google_IO.py
@@ -18,31 +18,43 @@ def SheetUI():
 ## https://github.com/gsuitedevs/PyDrive
 ## https://stackoverflow.com/questions/24419188/automating-pydrive-verification-process
 
+#### Authenticate only as needed ###
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("mycred.txt")
+if gauth.credentials is None:
+    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+elif gauth.access_token_expired:
+    gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+else:
+    gauth.Authorize() #Just run because everything is loaded properly
+gauth.SaveCredentialsFile("mycred.txt")
+drive=GoogleDrive(gauth)
+
+
 def DriveCreateFolder(title1):
-    gauth = GoogleAuth()
-    gauth.LoadCredentialsFile("mycred.txt")
-    if gauth.credentials is None:
-        gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-    elif gauth.access_token_expired:
-        gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-    else:
-        gauth.Authorize() #Just run because everything is loaded properly
-    gauth.SaveCredentialsFile("mycred.txt")
-    drive=GoogleDrive(gauth)
-#    folder_id = '1qoQXcyhaK7KE-iIxFoEhdnC_8Tg01gPb'
-    ## mimetype sets the type of object to a folder type
+    tgt_folder_id='13xmOpwh-uCiSeJn8pSktzMlr7BaPDo7B' ##Target Folder remains constant (for now)
+#    print("Target: %s" % tgt_folder_id)
     file_metadata = {
         'title': title1,
-        'mimeType': 'application/vnd.google-apps.folder'
+        "parents": [{"kind": "drive#fileLink","id": tgt_folder_id}],
+        'mimeType': 'application/vnd.google-apps.folder'   ## mimetype sets the type of object to a folder type
     }
     file = drive.CreateFile(file_metadata)
     file.Upload()
-    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+    print("Directory Created: " + "%s" %(title1))
+    file_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % tgt_folder_id}).GetList()
     for file1 in file_list:       
         if file1['title']==title1:
-            print ('title: %s, id: %s' % (file1['title'], file1['id']))
+            return(file1['id'])
         else:
             pass
 
-#DriveCreateFolder('test1')
-
+def DriveAddTemplates(opdir, RunID):
+#    template_folder='1OxuxqfumIpg3MPr3rgtRxwcOgCfetkIu'  ##Hardcoded as this template doesn't change
+    template_folder='1OxuxqfumIpg3MPr3rgtRxwcOgCfetkIu'  ##Hardcoded as this template doesn't change
+#    print("Template: %s" % template_folder)
+    file_template_list = drive.ListFile({'q': "'%s' in parents and trashed=false" % template_folder}).GetList()
+    for templatefile in file_template_list:       
+            basename=templatefile['title']
+            drive.auth.service.files().copy(fileId=templatefile['id'], body={"parents": [{"kind": "drive#fileLink", "id": opdir}], 'title': '%s_%s' %(RunID,basename)}).execute()
+            print("File Created: " + "%s_%s" %(RunID,basename))

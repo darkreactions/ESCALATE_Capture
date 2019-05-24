5b1d02a68e814d73557e1381df96c1e691b6348d
miketynes <mtynes95@gmail.com>
2019-05-24 15:09:57 -0400

    remove FAH from solvent list

--- capture/devconfig.py
+++ capture/devconfig.py
@@ -6,12 +6,12 @@ RoboVersion = 2.4
 # Hard coded limits
 max_robot_reagents = 7
 maxreagentchemicals = 3
-volspacing = 10 #reagent microliter (uL) spacing between points in the stateset 
+volspacing = 10 #reagent microliter (uL) spacing between points in the stateset
 
 
 # perovskite solvent list (simple specification of what is a liquid)
 # assumes only 1 liquid / reagent
-solventlist = ['GBL', 'DMSO', 'DMF', 'FAH', 'DCM'] #ya, I know FAH isn't a solvent, but it makes programming easier
+solventlist = ['GBL', 'DMSO', 'DMF', 'DCM']
 
 # lab file requirements list
 
